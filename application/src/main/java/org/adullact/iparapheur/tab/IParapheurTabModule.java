98302476fa81efcc8b4e7a025804ef25019877d7
Paul Merlin <paul@nosphere.org>
2012-04-30 15:47:52 +0200

    Introduced interface for IParapheurHttpClient

--- application/src/main/java/org/adullact/iparapheur/tab/IParapheurTabModule.java
+++ application/src/main/java/org/adullact/iparapheur/tab/IParapheurTabModule.java
@@ -1,7 +1,11 @@
 package org.adullact.iparapheur.tab;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
 import com.google.inject.name.Names;
+
+import org.adullact.iparapheur.tab.services.DefaultIParapheurHttpClient;
+import org.adullact.iparapheur.tab.services.IParapheurHttpClient;
 
 public class IParapheurTabModule
         extends AbstractModule
@@ -11,6 +15,8 @@ public class IParapheurTabModule
     protected void configure()
     {
         bind( String.class ).annotatedWith( Names.named( "androlog.filename" ) ).toInstance( "iparapheur-tab.properties" );
+
+        bind( IParapheurHttpClient.class ).to( DefaultIParapheurHttpClient.class ).in( Singleton.class );
     }
 
 }
--- application/src/main/java/org/adullact/iparapheur/tab/services/IParapheurHttpClient.java
+++ application/src/main/java/org/adullact/iparapheur/tab/services/IParapheurHttpClient.java
@@ -1,40 +1,13 @@
 package org.adullact.iparapheur.tab.services;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import com.google.inject.Singleton;
 
 import org.adullact.iparapheur.tab.model.Account;
 import org.adullact.iparapheur.tab.model.Office;
 
-// TODO Introduce interface (needs guice binding definition)
-@Singleton
-public class IParapheurHttpClient
+public interface IParapheurHttpClient
 {
 
-    public List<Office> getOffices( Account account )
-    {
-        // TODO REMOVE BEGIN
-        try {
-            Thread.sleep( 500 );
-        } catch ( InterruptedException ex ) {
-            // Ignored
-        }
-        List<Office> offices = new ArrayList<Office>();
-        offices.add( new Office( account.getTitle() + "-A1", "Bureau A/1", account.getTitle() + " A" ) );
-        offices.add( new Office( account.getTitle() + "-A2", "Bureau A/2", account.getTitle() + " A" ) );
-        offices.add( new Office( account.getTitle() + "-A3", "Bureau A/3", account.getTitle() + " A" ) );
-        offices.add( new Office( account.getTitle() + "-B1", "Bureau B/1", account.getTitle() + " B" ) );
-        offices.add( new Office( account.getTitle() + "-B2", "Bureau B/2", account.getTitle() + " B" ) );
-        // TODO REMOVE ENDS
-
-        return offices;
-    }
-
-    public String foo()
-    {
-        return "bar";
-    }
+    List<Office> getOffices( Account account );
 
 }
