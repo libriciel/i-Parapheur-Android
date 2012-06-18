628e7be3f9790d234fef1093e12416af109b1970
Paul Merlin <paul@nosphere.org>
2012-06-18 11:44:38 +0200

    Added build info generation and about dialog.

--- application/src/main/java/org/adullact/iparapheur/tab/ui/actionbar/ActionBarActivityObserver.java
+++ application/src/main/java/org/adullact/iparapheur/tab/ui/actionbar/ActionBarActivityObserver.java
@@ -1,9 +1,15 @@
 package org.adullact.iparapheur.tab.ui.actionbar;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
 
 import roboguice.event.Observes;
 import roboguice.inject.ContextSingleton;
@@ -15,6 +21,7 @@ import de.akquinet.android.androlog.Log;
 import org.codeartisans.android.toolbox.activity.event.OnCreateOptionsMenuEvent;
 import org.codeartisans.android.toolbox.activity.event.OnOptionsItemSelectedEvent;
 
+import org.adullact.iparapheur.tab.IParapheurTabBuildInfo;
 import org.adullact.iparapheur.tab.R;
 import org.adullact.iparapheur.tab.ui.Refreshable;
 import org.adullact.iparapheur.tab.ui.dashboard.DashboardActivity;
@@ -32,6 +39,7 @@ public class ActionBarActivityObserver
      */
     public void handleOnCreateOptionsMenu( @Observes OnCreateOptionsMenuEvent event )
     {
+        // Refresh
         Menu menu = event.getMenu();
         if ( activity instanceof Refreshable ) {
             MenuItem refresh = menu.add( "Rafraichir" );
@@ -48,6 +56,8 @@ public class ActionBarActivityObserver
 
             } );
         }
+
+        // Accounts
         MenuItem accounts = menu.add( "Comptes" );
         accounts.setIcon( R.drawable.ic_accounts ); // Won't be shown as Android don't want icons in the overflow menu
         accounts.setShowAsAction( MenuItem.SHOW_AS_ACTION_NEVER );
@@ -57,6 +67,45 @@ public class ActionBarActivityObserver
             public boolean onMenuItemClick( MenuItem mi )
             {
                 activity.startActivity( new Intent( activity, AccountsActivity.class ) );
+                return true;
+            }
+
+        } );
+
+        // About
+        MenuItem about = menu.add( "À propos" );
+        about.setIcon( R.drawable.icon );
+        about.setShowAsAction( MenuItem.SHOW_AS_ACTION_NEVER );
+        about.setOnMenuItemClickListener( new MenuItem.OnMenuItemClickListener()
+        {
+
+            public boolean onMenuItemClick( MenuItem mi )
+            {
+                View aboutLayout = activity.getLayoutInflater().inflate( R.layout.about_dialog, null );
+                WebView aboutWebView = ( WebView ) aboutLayout.findViewById( R.id.about_webview );
+                aboutWebView.loadUrl( "file:///android_asset/about/index.html" );
+
+                StringBuilder title = new StringBuilder();
+                title.append( IParapheurTabBuildInfo.DESCRIPTION ).
+                        append( " - " ).
+                        append( IParapheurTabBuildInfo.VERSION ).
+                        append( " - " ).
+                        append( new SimpleDateFormat( "dd/MM/yyyy" ).format( new Date( IParapheurTabBuildInfo.BUILD_TIMESTAMP ) ) );
+
+                AlertDialog.Builder builder = new AlertDialog.Builder( activity );
+                builder.setTitle( title.toString() );
+                builder.setIcon( R.drawable.icon );
+                builder.setView( aboutLayout );
+                builder.setPositiveButton( "Fermer", new DialogInterface.OnClickListener()
+                {
+
+                    public void onClick( DialogInterface dialog, int id )
+                    {
+                        dialog.cancel();
+                    }
+
+                } );
+                builder.show();
                 return true;
             }
 
