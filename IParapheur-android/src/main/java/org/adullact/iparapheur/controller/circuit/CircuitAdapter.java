f5cb8ae38b2e55175998cbf739a75aeada698cb9
Adrien Bricchi <adrien.bricchi@adullact-projet.coop>
2015-03-18 09:23:04 +0100

    Cleanup
    Former-commit-id: 671e51d099997e0052a897ec9b77341e483e9622

--- IParapheur-android/src/main/java/org/adullact/iparapheur/controller/circuit/CircuitAdapter.java
+++ IParapheur-android/src/main/java/org/adullact/iparapheur/controller/circuit/CircuitAdapter.java
@@ -13,29 +13,26 @@ import org.adullact.iparapheur.model.EtapeCircuit;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-/**
-* Created by jmaire on 13/01/2014.
-*/
-public class CircuitAdapter extends ArrayAdapter<EtapeCircuit>
-{
-    public CircuitAdapter(Context context, List<EtapeCircuit> circuit) {
-        super(context, R.layout.etape_circuit, R.id.etape_circuit_bureau, circuit);
-    }
+public class CircuitAdapter extends ArrayAdapter<EtapeCircuit> {
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View view = super.getView(position, convertView, parent);
-        EtapeCircuit etape = getItem(position);
-        ((ImageView) view.findViewById(R.id.etape_circuit_icon)).setImageResource(etape.getAction().getIcon(etape.isApproved()));
-        if (etape.isApproved()) {
-            SimpleDateFormat df = (SimpleDateFormat) SimpleDateFormat.getDateTimeInstance(SimpleDateFormat.SHORT, SimpleDateFormat.SHORT);
-            String signataire = etape.getSignataire();
-            String validation = validation = getContext().getResources().getString(R.string.le) + " " + df.format(etape.getDateValidation());
-            if ((signataire != null) && !signataire.trim().isEmpty() && !signataire.equalsIgnoreCase("null")) {
-                validation += " " + getContext().getResources().getString(R.string.par) + " " + signataire;
-            }
-            ((TextView) view.findViewById(R.id.etape_circuit_validation)).setText(validation);
-        }
-        return view;
-    }
+	public CircuitAdapter(Context context, List<EtapeCircuit> circuit) {
+		super(context, R.layout.etape_circuit, R.id.etape_circuit_bureau, circuit);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View view = super.getView(position, convertView, parent);
+		EtapeCircuit etape = getItem(position);
+		((ImageView) view.findViewById(R.id.etape_circuit_icon)).setImageResource(etape.getAction().getIcon(etape.isApproved()));
+		if (etape.isApproved()) {
+			SimpleDateFormat df = (SimpleDateFormat) SimpleDateFormat.getDateTimeInstance(SimpleDateFormat.SHORT, SimpleDateFormat.SHORT);
+			String signataire = etape.getSignataire();
+			String validation = validation = getContext().getResources().getString(R.string.le) + " " + df.format(etape.getDateValidation());
+			if ((signataire != null) && !signataire.trim().isEmpty() && !signataire.equalsIgnoreCase("null")) {
+				validation += " " + getContext().getResources().getString(R.string.par) + " " + signataire;
+			}
+			((TextView) view.findViewById(R.id.etape_circuit_validation)).setText(validation);
+		}
+		return view;
+	}
 }
