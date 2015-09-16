55a8498238df07afde02f182bd757376e6f49bc3
Adrien Bricchi <adrien.bricchi@adullact-projet.coop>
2015-09-16 11:49:27 +0200

    Themes & card (first draft)
    Former-commit-id: 5feb7133aaabfcc17074bc4db7799301c8690341

--- IParapheur-android/src/main/java/org/adullact/iparapheur/controller/preferences/PreferencesActivity.java
+++ IParapheur-android/src/main/java/org/adullact/iparapheur/controller/preferences/PreferencesActivity.java
@@ -3,11 +3,15 @@ package org.adullact.iparapheur.controller.preferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
 import org.adullact.iparapheur.R;
 
-public class PreferencesActivity extends AppCompatActivity {
+public class PreferencesActivity extends AppCompatActivity implements PreferencesMenuFragment.PreferenceMenuFragmentListener {
+
+	// <editor-fold desc="LifeCycle">
 
 	@Override protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,5 +28,24 @@ public class PreferencesActivity extends AppCompatActivity {
 		if (getSupportActionBar() != null)
 			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
+
+	// </editor-fold desc="LifeCycle">
+
+	// <editor-fold desc="ActionBar">
+
+	@Override public boolean onOptionsItemSelected(MenuItem item) {
+		onBackPressed();
+		return true;
+	}
+
+	// </editor-fold desc="ActionBar">
+
+	// <editor-fold desc="PreferenceMenuFragmentListener">
+
+	@Override public void onMenuElementClicked(String element) {
+		Log.i("Adrien", "Click");
+	}
+
+	// </editor-fold desc="PreferenceMenuFragmentListener">
 
 }
---
+++ IParapheur-android/src/main/java/org/adullact/iparapheur/controller/preferences/PreferencesMenuFragment.java
@@ -0,0 +1,98 @@
+package org.adullact.iparapheur.controller.preferences;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import org.adullact.iparapheur.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link PreferenceMenuFragmentListener} interface
+ * to handle interaction events.
+ * Use the {@link PreferencesMenuFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PreferencesMenuFragment extends Fragment {
+
+	private ListView mListView;
+	private PreferenceMenuFragmentListener mListener;
+
+	/**
+	 * Use this factory method to create a new instance of
+	 * this fragment using the provided parameters.
+	 *
+	 * @return A new instance of fragment PreferencesMenuFragment.
+	 */
+	public static PreferencesMenuFragment newInstance() {
+		PreferencesMenuFragment fragment = new PreferencesMenuFragment();
+		Bundle args = new Bundle();
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+	public PreferencesMenuFragment() {
+		// Required empty public constructor
+	}
+
+	// <editor-fold desc="LifeCycle">
+
+	@Override public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		View view = inflater.inflate(R.layout.fragment_preferences_menu, container, false);
+		mListView = (ListView) view.findViewById(R.id.preferences_activity_general_list);
+		return view;
+	}
+
+	@Override public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		try {
+			mListener = (PreferenceMenuFragmentListener) activity;
+		}
+		catch (ClassCastException e) {
+			throw new ClassCastException(
+					activity.toString() + " must implement OnFragmentInteractionListener"
+			);
+		}
+	}
+
+	@Override public void onDetach() {
+		super.onDetach();
+		mListener = null;
+	}
+
+	// </editor-fold desc="LifeCycle">
+
+	public void onMenuElementClicked(Uri uri) {
+
+		if (mListener != null)
+			mListener.onMenuElementClicked("" + uri);
+	}
+
+	/**
+	 * This interface must be implemented by activities that contain this
+	 * fragment to allow an interaction in this fragment to be communicated
+	 * to the activity and potentially other fragments contained in that
+	 * activity.
+	 * <p/>
+	 * See the Android Training lesson <a href=
+	 * "http://developer.android.com/training/basics/fragments/communicating.html"
+	 * >Communicating with Other Fragments</a> for more information.
+	 */
+	public interface PreferenceMenuFragmentListener {
+
+		// TODO: Update argument type and name
+		void onMenuElementClicked(String element);
+	}
+
+}
