fbeeb5642070c67ec349c785819a025f8696bb85
Adrien Bricchi <adrien.bricchi@adullact-projet.coop>
2015-10-22 09:52:50 +0200

    Updates and compilation fix
    Former-commit-id: d8763d719b7c726815a9649fe9b1a23af9fff1b5

--- IParapheur-android/src/main/java/org/adullact/iparapheur/controller/rest/api/RestClientAPI4.java
+++ IParapheur-android/src/main/java/org/adullact/iparapheur/controller/rest/api/RestClientAPI4.java
@@ -4,18 +4,17 @@ import android.support.annotation.NonNull;
 
 import java.util.Locale;
 
+
 public class RESTClientAPI4 extends RESTClientAPI3 {
 
 	private static final String RESOURCE_ANNOTATIONS = "/parapheur/dossiers/%s/%s/annotations";
 	private static final String RESOURCE_ANNOTATION = "/parapheur/dossiers/%s/%s/annotations/%s";
 
-	@Override
-	protected @NonNull String getAnnotationsUrlSuffix(@NonNull String dossierId, @NonNull String documentId) {
+	@Override protected @NonNull String getAnnotationsUrlSuffix(@NonNull String dossierId, @NonNull String documentId) {
 		return String.format(Locale.US, RESOURCE_ANNOTATIONS, dossierId, documentId);
 	}
 
-	@Override
-	protected @NonNull String getAnnotationUrlSuffix(@NonNull String dossierId, @NonNull String documentId, @NonNull String annotationId) {
+	@Override protected @NonNull String getAnnotationUrlSuffix(@NonNull String dossierId, @NonNull String documentId, @NonNull String annotationId) {
 		return String.format(Locale.US, RESOURCE_ANNOTATION, dossierId, documentId, annotationId);
 	}
 
