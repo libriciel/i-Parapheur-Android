185858246500804f0c825ca97f3edb872ec603c2
Adrien Bricchi <adrien.bricchi@adullact-projet.coop>
2016-09-06 17:58:34 +0200

    Account unit tests
    Former-commit-id: ca4a55fdbfc46598dbd02c838d1701f80c3552c4

--- IParapheur-android/src/androidTest/java/org/adullact/iparapheur/controller/account/MyAccountsTest.java
+++ IParapheur-android/src/androidTest/java/org/adullact/iparapheur/controller/account/MyAccountsTest.java
@@ -17,24 +17,116 @@
  */
 package org.adullact.iparapheur.controller.account;
 
+import android.support.annotation.NonNull;
 import android.support.test.filters.MediumTest;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Log;
 
-import org.junit.Before;
+import junit.framework.Assert;
+
+import org.adullact.iparapheur.model.Account;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.List;
 
+
+/**
+ * @see org.adullact.iparapheur.controller.account.MyAccounts
+ */
 @RunWith(AndroidJUnit4.class)
 @MediumTest
 public class MyAccountsTest {
 
-	@Before public void init() {
+	// <editor-fold desc="Static utils">
+
+	private static @NonNull Account createAccount(@NonNull String number) {
+
+		Account account = MyAccounts.INSTANCE.addAccount();
+		account.setServerBaseUrl("baseurl" + number + ".org");
+		account.setTitle("title_" + number);
+		account.setLogin("login_" + number);
+		account.setPassword("password_@\"!()\\_" + number);
+
+		return account;
 	}
 
-	@Test public void demo_test() {
-		Log.v("Adrien", "here1");
+	private static void cleanupData() {
+
+		List<Account> accountList = new ArrayList<>(MyAccounts.INSTANCE.getAccounts());
+		for (Account account : accountList)
+			MyAccounts.INSTANCE.delete(account);
 	}
 
+	// </editor-fold desc="Static utils">
+
+	@Test public void testSave() {
+
+		cleanupData();
+
+		createAccount("01");
+		Assert.assertEquals(MyAccounts.INSTANCE.getAccounts().size(), 1);
+
+		createAccount("02");
+		Assert.assertEquals(MyAccounts.INSTANCE.getAccounts().size(), 2);
+
+		createAccount("03");
+		Assert.assertEquals(MyAccounts.INSTANCE.getAccounts().size(), 3);
+
+		cleanupData();
+	}
+
+	@Test public void testDelete() {
+
+		// Create
+
+		cleanupData();
+
+		createAccount("01");
+		createAccount("02");
+		createAccount("03");
+
+		// Delete and test
+
+		Assert.assertEquals(MyAccounts.INSTANCE.getAccounts().size(), 3);
+
+		List<Account> accountList = new ArrayList<>(MyAccounts.INSTANCE.getAccounts());
+		for (Account account : accountList)
+			MyAccounts.INSTANCE.delete(account);
+
+		Assert.assertEquals(MyAccounts.INSTANCE.getAccounts().size(), 0);
+	}
+
+	@Test public void testGetAccount() {
+
+		// Create
+
+		cleanupData();
+
+		Account inputAccount01 = createAccount("01");
+		Account inputAccount02 = createAccount("02");
+
+		// Fetch and test
+
+		MyAccounts.INSTANCE.getAccounts();
+
+		Account savedAccount01 = MyAccounts.INSTANCE.getAccount(inputAccount01.getId());
+		Assert.assertNotNull(savedAccount01);
+		Assert.assertEquals(savedAccount01.getId(), inputAccount01.getId());
+		Assert.assertEquals(savedAccount01.getServerBaseUrl(), inputAccount01.getServerBaseUrl());
+		Assert.assertEquals(savedAccount01.getTitle(), inputAccount01.getTitle());
+		Assert.assertEquals(savedAccount01.getLogin(), inputAccount01.getLogin());
+		Assert.assertEquals(savedAccount01.getPassword(), inputAccount01.getPassword());
+
+		Account savedAccount02 = MyAccounts.INSTANCE.getAccount(inputAccount02.getId());
+		Assert.assertNotNull(savedAccount02);
+		Assert.assertEquals(savedAccount02.getId(), inputAccount02.getId());
+		Assert.assertEquals(savedAccount02.getServerBaseUrl(), inputAccount02.getServerBaseUrl());
+		Assert.assertEquals(savedAccount02.getTitle(), inputAccount02.getTitle());
+		Assert.assertEquals(savedAccount02.getLogin(), inputAccount02.getLogin());
+		Assert.assertEquals(savedAccount02.getPassword(), inputAccount02.getPassword());
+
+		cleanupData();
+	}
 }
+
